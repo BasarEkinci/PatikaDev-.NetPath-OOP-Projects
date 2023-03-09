8a9da9ffff0b9854b1317d4ad796242888ad5800
BasarEkinci <basarekinci02@outlook.com>
2023-03-09 20:37:12 +0300

    Algorithm

---
+++ EasyLevel/Algorithm/Algorithm/Algorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Algorithm
+    {
+        public static void RemoveLetter()
+        {
+            Console.Write("Please enter a string and index value: ");
+            string input = Console.ReadLine();
+
+
+            string[] parts = input.Split(',');
+            string str = parts[0].Trim();
+            int index = int.Parse(parts[1].Trim());
+
+            if (index < 0 || index >= str.Length)
+            {
+                Console.WriteLine("Error: Invalid index value");
+                return;
+            }
+            string output = str.Remove(index, 1);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ EasyLevel/Algorithm/Algorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Algorithm.RemoveLetter();
+            Console.ReadLine();
+        }
+    }
+}

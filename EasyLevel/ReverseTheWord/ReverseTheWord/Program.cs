f3aab25445762b0a5e5e47fa2c276eafcfbd7d6e
BasarEkinci <basarekinci02@outlook.com>
2023-03-09 22:14:12 +0300

    Revers the Word

---
+++ EasyLevel/ReverseTheWord/ReverseTheWord/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseTheWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseTheWord("Hello World");
+            Console.ReadLine();
+        }
+        public static void ReverseTheWord(string str)
+        {
+            string reverseString = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseString += str[i];
+            }
+
+            Console.WriteLine("Entered Word: " + str);
+            Console.WriteLine("Reversed Word: " + reverseString);
+        }
+    }
+}

899a994e5bf46015507577b38d89f983019a2a0d
BasarEkinci <basarekinci02@outlook.com>
2023-03-09 16:50:19 +0300

    Draw Circle

---
+++ EasyLevel/DrawCircle/DrawCircle/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    internal class Circle
+    {
+        public static void DrawCircle(int radius)
+        {
+            double angle, x, y;
+
+            for (y = radius; y >= -radius; --y)
+            {
+                for (x = -radius; x <= radius; x += 0.5)
+                {
+                    if (radius - 0.5 < Math.Sqrt(x * x + y * y) && Math.Sqrt(x * x + y * y) < radius + 0.5)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EasyLevel/DrawCircle/DrawCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the radius of circle: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            Circle.DrawCircle(radius);
+            Console.ReadLine();
+        }
+    }
+}

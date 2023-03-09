b14b875f33455968b535664cab1b79c36782ec66
BasarEkinci <basarekinci02@outlook.com>
2023-03-09 16:29:29 +0300

    Drawing Triangle

---
+++ EasyLevel/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the height of the Triangle: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Triangle.DrawTriangle(height);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EasyLevel/DrawTriangle/DrawTriangle/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    internal class Triangle
+    {
+        public static void DrawTriangle(int size)
+        {
+            int row, space;
+            for(row = 1; row <= size;row++)
+            {
+                //Add spaces
+                for (space = 1; space <= (size - row); space++)
+                {
+                    Console.Write(" ");
+                }
+                //Add stars
+                for(int i = 1; i <= (2 * row - 1); i++)
+                {
+                    Console.Write("*");
+                }
+                //Skip to next line
+                Console.WriteLine();
+            }
+        }
+    }
+}

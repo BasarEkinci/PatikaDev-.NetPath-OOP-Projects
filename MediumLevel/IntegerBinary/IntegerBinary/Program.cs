3de9ffb546cf657cd716ae734e24a5c8460d0e66
BasarEkinci <basarekinci02@outlook.com>
2023-03-10 12:45:28 +0300

    Integer Binary

---
+++ MediumLevel/IntegerBinary/IntegerBinary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers do you want to enter (Must be a even number!) ?");
+            Console.Write("====> ");
+            int sayi = int.Parse(Console.ReadLine());
+            if(sayi % 2 == 0) 
+            {
+                int[] numberArray = new int[sayi];
+
+                for (int i = 0; i < numberArray.Length; i++)
+                {
+                    Console.Write((i + 1) + "th number: ");
+                    int number = int.Parse(Console.ReadLine());
+                    numberArray[i] = number;
+                }
+
+                for (int i = 0; i < numberArray.Length; i+=2)
+                {
+                    if (numberArray[i] == numberArray[i + 1])
+                    {
+                        int sum = numberArray[i] + numberArray[i + 1];
+                        Console.Write(" " + (sum * sum) + " ");
+                    }
+                    else
+                    {
+                        int sum = numberArray[i] + numberArray[i + 1];
+                        Console.Write(" " + sum + " ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.Please enter even number!");
+            }
+           Console.ReadLine();
+        }
+              
+    }
+
+}
---
+++ MediumLevel/IntegerBinary/IntegerBinary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IntegerBinary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegerBinary")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07cab150-3595-40a4-a45a-5e24f2f4e134")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
